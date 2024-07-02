eaeb8a9a85e1b3895fd6b9a055c0c8c4a10d4f5e
Dipokal HHJ <hhj@devent.kr>
2024-07-02 22:23:28 +0900

    feat: add langchain

--- README.md
+++ README.md
@@ -2,6 +2,12 @@
 
 FastAPI로 구현된 랭체인 프로젝트 입니다.
 
+아래 환경 변수가 필요합니다. CI/CD 배포 옵션에서 변경 가능합니다.
+
+`OPENAI_API_KEY`
+
+아래 명령을 통해 앱을 실행시킬 수 있습니다.
+
 ```
 uvicorn main:app --reload
 ```
--- main.py
+++ main.py
@@ -1,11 +1,26 @@
 from typing import Union
-
 from fastapi import FastAPI
+import getpass
+import os
+from langchain_openai import ChatOpenAI
+from langchain_core.messages import HumanMessage
+
+os.environ["LANGCHAIN_TRACING_V2"] = "true"
+os.environ["LANGCHAIN_API_KEY"] = getpass.getpass()
+os.environ["OPENAI_API_KEY"] = ""
+
+
+model = ChatOpenAI(model="gpt-3.5-turbo")
 
 app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    return {"version": "0.0.1"}
 
+
+@app.get("/send")
+def read_root(content: str = ""):
+    response = model.invoke([HumanMessage(content=content)])
+    return {"content": response.content}
