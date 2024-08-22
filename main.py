4f6bfb0b71a79e4e970d56520b0d3fbc546f1508
Dipokal HHJ <hhj@devent.kr>
2024-08-22 16:04:47 +0900

    fix: update file

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage
 
 os.environ["LANGCHAIN_TRACING_V2"] = "true"
-os.environ["LANGCHAIN_API_KEY"] = getpass.getpass()
 
 
 model = ChatOpenAI(model="gpt-3.5-turbo")
